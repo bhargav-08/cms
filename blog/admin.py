cdb06c5b51efdcd6452a86ce5b80f2f2bc164496
bhargav08 <bhargavchandpara18@gmail.com>
2021-11-30 11:51:19 +0530

    Initial Commit

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from blog.models import Posts
+
+# Register your models here.
+
+admin.site.register(Posts)
---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Posts
+# Create your views here.
+
+
+
+def home(request):
+    context = {
+        'posts' : Posts.objects.all()
+    }  
+    return render(request, 'blog/home.html', context = context)
+
+def about(request):
+    return render(request,'blog/about.html',{"title":"About"})
